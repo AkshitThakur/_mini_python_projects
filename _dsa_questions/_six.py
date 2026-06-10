8c2b9ed66e3d047f85ff8e0ec8c9198ff5e1db3f
Askhit Thakur <akshitthakur2425@gmail.com>
2026-06-10 16:49:24 +0530

    Python Program to find smallest K-digit number divisible by X.

---
+++ _dsa_questions/_six.py
@@ -0,0 +1,19 @@
+# Python Program to find smallest K-digit number divisible by X.
+#83 => 10000
+#83*120 = 9960
+#83*121 = 10043
+
+"""def k_digit(K, X):
+    _small_k_digit_num = 10**(K-1) #10000
+    _rem = _small_k_digit_num%X #40
+    _small_k_digit_num += X-_rem
+    return _small_k_digit_num"""
+
+def k_digit(K, X):
+    _small_k_digit_num = 10**(K-1)
+    return (
+        _small_k_digit_num + (-_small_k_digit_num%X)#rem = 43
+    )
+
+_result = k_digit(5, 83)
+print(_result)
